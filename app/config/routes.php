3e5d84ab6ee7c33797b15283910698efccdc113f
issdilshod <dilshod@themembers.com>
2022-05-08 14:02:38 +0300

    Task #2: Make router for app

---
+++ app/config/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'project' => [
+        'controller' => 'account',
+        'action' => 'login'
+    ], 
+
+    'project/login' => [
+        'controller' => 'account',
+        'action' => 'login'
+    ], 
+
+    'project/dashboard' => [
+        'controller' => 'dashboard',
+        'action' => 'index'
+    ], 
+];
--- app/core/Router.php
+++ app/core/Router.php
@@ -8,19 +8,39 @@ class Router{
     protected $params = [];
 
     public function __construct(){
-        //
+        $arr = require_once 'app/config/routes.php';
+        foreach ($arr as $key => $val){
+            $this->add($key, $val);
+        }
     }
 
     public function add($route, $param){
-        //
+        $route = '#^' . $route . '$#';
+        $this->routes[$route] = $param;
     }
 
     public function match(){
-        //
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach($this->routes as $route => $param){
+            if (preg_match($route, $url, $matches)){
+                $this->params = $param;
+                return true;
+            }
+        }
+        return false;
     }
 
     public function run(){
-        //
+        if($this->match()){
+            $controller = 'app\controllers\\'. ucfirst($this->params['controller']).'Controller.php';
+            if (class_exists($controller)){
+                
+            }else{
+                // Class not found
+            }
+        }else{
+            // Route not found
+        }
     }
 
 }
