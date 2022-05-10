a71639452a997170dc665f873ef6f3f1697df880
issdilshod <dilshod@themembers.com>
2022-05-10 23:04:09 +0300

    Task #6: Add class db, connect to db

---
+++ app/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'project',
+    'port' => '3306',
+    'charset' => 'utf8'
+];
--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -2,13 +2,19 @@
 namespace app\controllers;
 
 use app\core\Controller;
+use app\lib\Db;
 
 class AccountController extends Controller{
 
     public function loginAction(){
+        // $db = new Db();
+        // $vars = $db->query("SELECT * FROM users")->rows;
+
         // Another path to layout
         // $this->view->layout = 'custom';
+
         $this->view->render('Login');
+
         // Redirect to another link
         // $this->view->redirect($url);
     }
